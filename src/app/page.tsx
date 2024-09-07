6d9a25ae98d9945a87013337b398584c6e24915d
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-09-07 17:16:53 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
+'use client'; 
+
+import { useState, useEffect } from 'react';
 import TodoList from '../components/TodoList';
 import CreateTodo from '../components/CreateTodo';
 import SearchTodo from '../components/SearchTodo';
 import { getTodos } from '../lib/todoService';
 
-export default async function Home() {
-  const { todos } = await getTodos(); 
+export default function Home() {
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() => {
+    async function fetchTodos() {
+      const { todos } = await getTodos(); 
+      setTodos(todos); // Set the todos to state
+    }
+    fetchTodos();
+  }, []);
+
+  const addTodo = (newTodo: any) => {
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
 
   return (
     <div>
-      <CreateTodo />
+      <CreateTodo onAddTodo={addTodo} />
       <SearchTodo initialTodos={todos} />
-      
     </div>
   );
 }
