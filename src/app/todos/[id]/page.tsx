a76cd4d052e41ec0ab4a262ba85c68c516441403
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-09-07 13:49:48 +0100

    Update page.tsx

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -4,7 +4,7 @@ export default async function TodoDetail({ params }: { params: { id: string } })
   const todo = await getTodo(params.id);
 
   return (
-    <div className="p-4 bg-secondary rounded">
+    <div className="p-4 bg-secondary rounded-md">
       {todo && <h1 className="text-xl font-bold">{todo.todo}</h1>}
     </div>
   );
