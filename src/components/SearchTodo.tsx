a10b644f28de4087a9b1caaee7861ab38225a931
Efemena <88979259+Festivemena@users.noreply.github.com>
2024-09-07 16:49:23 +0100

    Update SearchTodo.tsxk

--- src/components/SearchTodo.tsx
+++ src/components/SearchTodo.tsx
@@ -2,7 +2,8 @@
 
 import { useState, useEffect } from 'react';
 import { Input } from '@/components/ui/input';
-import Link from 'next/link'
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function SearchTodo({ initialTodos }: { initialTodos: any[] }) {
   const [searchTerm, setSearchTerm] = useState('');
